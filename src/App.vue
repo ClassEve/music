f12191f1948164692721e9c8d7c9e7a495de37cb
wudong <wudong@hey900.com>
2017-10-23 17:21:01 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    htllo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
+</style>
