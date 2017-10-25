9fd3bd78e225b5efc4a39e906680d2a5dcc333d9
wudong <wudong@hey900.com>
2017-10-25 17:53:00 +0800

    10%

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="slider" ref="slider">
-    <div class="slider-group">
+    <div class="slider-group" ref="sliderGroup">
       <slot>
       </slot>
     </div>
     <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :class="{active: currentPageIndex === index}"></span>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-//  import {addClass} from 'common/js/dom'
+  import {addClass} from 'common/js/dom'
   import BScroll from 'better-scroll'
 
   export default {
@@ -21,15 +22,94 @@
       },
       autoPlay: {
         type: Boolean,
-        default: true
+        default: false
       },
       interval: {
         type: Number,
         default: 4000
       }
     },
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
     mounted() {
+      setTimeout(() => {    // 浏览器17毫秒刷新次
+        this._setSliderWidth()
+        this._initDots()
+        this._initSlider()
 
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 20)
+      window.addEventListener('resize', () => {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()
+      })
+    },
+    methods: {
+      _setSliderWidth(isResize) {
+        this.children = this.$refs.sliderGroup.children
+
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+
+        if (this.loop && !isResize) {
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        })
+
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) {
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _play() {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        }, this.interval)
+      }
+    },
+    destroyed() { // 单页面有定时器时，被切走后，清空定时器，释放内存
+      clearTimeout(this.timer)
     }
   }
 </script>
