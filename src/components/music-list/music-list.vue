4d5a0be61a036472265d764e7676e541c8e443d0
wudong <wudong@hey900.com>
2017-11-16 18:31:45 +0800

    musiclist done

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length > 0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -20,6 +26,10 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
+
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop')
 
   const RESERVED_HEIGHT = 40
   export default {
@@ -52,6 +62,9 @@
       this.listenScroll = true
     },
     methods: {
+      back() {
+        this.$router.back()
+      },
       scroll(pos) {
         this.scrollY = pos.y
       }
@@ -65,18 +78,32 @@
       scrollY(newY) {
         let translateY = Math.max(this.minTranslateY, newY)
         let zIndex = 0
-        this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-        this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+        let scale = 1
+        let blur = 0
+
+        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
 
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style['height'] = `40px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     components: {
