4b458fa88b2eb3986c18a424801d84a3d252c2c8
ClassName <wdd667@outlook.com>
2017-11-26 10:38:28 +0800

    store模型设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playList: [],
+  sequenceList: [], // 播放顺序
+  mode: playMode.sequence,
+  currentIndex: -1 // 当前播放哪首歌
 }
 
 export default state
